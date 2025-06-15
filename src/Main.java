5e5b41fd332b83b2d3193e28384b28fda88e46af
Diego Brunari <diego.skp@hotmail.com>
2025-06-15 01:28:33 -0300

    added java.nio2 with another example to the same problem

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,29 @@
 import br.com.dio.persistence.FilePersistence;
-import br.com.dio.persistence.NIOFilePersistence;
+import br.com.dio.persistence.NIO2FilePersistence;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        FilePersistence persistence = new NIOFilePersistence("user.csv");
-        System.out.println(persistence.write("diego;diego@email.com;17/07/1997"));
-        System.out.println("=============");
-        System.out.println(persistence.write("bernardo;bernardo@email.com;20/02/2002"));
-        System.out.println("=============");
-        System.out.println(persistence.write("suelen;su@email.com;12/12/1982"));
-        System.out.println("=============");
+        FilePersistence persistence = new NIO2FilePersistence("user.csv");
+        System.out.println("=========");
+        System.out.println(persistence.write("diego;diego@email.com;1997"));
+        System.out.println("=========");
+        System.out.println(persistence.write("maria;maria@email.com;2000"));
+        System.out.println("=========");
+        System.out.println(persistence.write("yesmim;yas@email.com;1999"));
+        System.out.println("=========");
         System.out.println(persistence.findAll());
-        System.out.println("=============");
-        System.out.println(persistence.findBy("suelen"));
-        System.out.println("=============");
-        System.out.println(persistence.findBy("/97"));
-        System.out.println("=============");
-        System.out.println(persistence.remove("bernardo"));
-        System.out.println("=============");
+        System.out.println("=========");
+        System.out.println(persistence.findBy("1999"));
+        System.out.println("=========");
+        System.out.println(persistence.remove("maria"));
+        System.out.println("=========");
         System.out.println(persistence.findAll());
-        System.out.println("=============");
-        System.out.println(persistence.replace("su@email", "outroteste;teste@email.com;10/10/2010"));
-        System.out.println("=============");
+        System.out.println("=========");
+        System.out.println(persistence.replace("yesmim", "YASMIM;yas@email.com;1999"));
+        System.out.println("=========");
         System.out.println(persistence.findAll());
-        System.out.println("=============");
+        System.out.println("=========");
     }
 }
---
+++ src/br/com/dio/persistence/NIO2FilePersistence.java
@@ -0,0 +1,99 @@
+package br.com.dio.persistence;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class NIO2FilePersistence implements FilePersistence{
+
+    private final String currentDir = System.getProperty("user.dir");
+    private final String storedDir = "/managerFiles/NIO2/";
+    private final String fileName;
+
+    public NIO2FilePersistence(String fileName) throws IOException {
+        this.fileName = fileName;
+        var path = Paths.get(currentDir + storedDir);
+        if(!Files.exists(path)){
+            Files.createDirectory(path);
+        }
+        clearFile();
+    }
+
+    @Override
+    public String write(String data) {
+        var path = Paths.get(currentDir + storedDir + fileName);
+        try {
+            Files.write(path, data.getBytes(), StandardOpenOption.APPEND);
+            Files.write(path, System.lineSeparator().getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return data;
+    }
+
+    @Override
+    public boolean remove(String sentence) {
+        var contentList = toListString();
+
+        if(contentList.stream().noneMatch(c -> c.contains(sentence))) return false;
+
+        clearFile();
+        contentList.stream()
+                .filter(c -> !c.contains(sentence))
+                .forEach(this::write);
+        return true;
+    }
+
+    @Override
+    public String replace(String oldContent, String newContent) {
+        var contentList = toListString();
+
+        if(contentList.stream().noneMatch(c -> c.contains(oldContent))) return "";
+
+        clearFile();
+        contentList.stream()
+                .map(c -> c.contains(oldContent) ? newContent : c)
+                .forEach(this::write);
+        return newContent;
+    }
+
+    @Override
+    public String findAll() {
+        var path = Paths.get(currentDir + storedDir + fileName);
+        var content = "";
+        try(var lines = Files.lines(path)){
+            content = lines.collect(Collectors.joining(System.lineSeparator()));
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+        return content;
+    }
+
+    @Override
+    public String findBy(String sentence) {
+        var content = findAll();
+        return Stream.of(content.split(System.lineSeparator()))
+                .filter(c -> c.contains(sentence))
+                .findFirst()
+                .orElse("");
+    }
+
+    private List<String> toListString() {
+        var content = findAll();
+        return new ArrayList<>(Stream.of(content.split(System.lineSeparator())).toList());
+    }
+
+    private void clearFile(){
+        try(OutputStream outputStream = new FileOutputStream(currentDir + storedDir + fileName)){
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
